4ce1d1f0c208ac8a60765605e823e1e2c42e84d0
Andres Carrillo <andresjch2804@gmail.com>
2019-08-11 19:32:43 -0500

    Add new packages

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,26 @@ const mix = require('laravel-mix');
  |
  */
 
+mix.webpackConfig({
+    resolve: {
+        extensions: ['.js', '.vue', '.json'],
+        alias: {
+            '@': __dirname + '/resources/js'
+        },
+    },
+});
+
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
+
+if (mix.inProduction()) {
+    mix.version();
+} else {
+    mix.browserSync({
+        proxy: 'divisionestudios.test/app',
+        files: [
+            'resources/**/*',
+        ],
+        browser: ['google chrome']
+    });
+}
