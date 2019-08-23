ec80beb1a0ef62831422b7d0b526b13be914d9d1
Andres Carrillo <andresjch2804@gmail.com>
2019-08-23 18:17:55 -0500

    Login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.app')
 
+@section('extra-head')
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+@endsection
+
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
@@ -70,4 +74,8 @@
             </div>
         </div>
     </div>
+@endsection
+
+@section('beforeScripts')
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 @endsection
