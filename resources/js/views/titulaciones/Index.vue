6a2f893f2d32dd827d1d6ffe972f39fc3586ec9c
Andres Carrillo <andresjch2804@gmail.com>
2019-08-24 20:30:05 -0500

    Refactoriza el index de nombramientos

--- resources/js/views/titulaciones/Index.vue
+++ resources/js/views/titulaciones/Index.vue
@@ -1,17 +1,26 @@
 <template>
     <v-container fluid style="min-height: 500px!important;">
-        <v-row v-if="!loading">
-            <template v-for="item in titulaciones">
-                <v-col cols="12" md="4" sm="6" xl="3">
-                    <nombramiento-card :nombramiento="item"></nombramiento-card>
+        <template v-if="lengthNombramientos">
+            <v-row v-if="!loading">
+                <template v-for="item in titulaciones">
+                    <v-col cols="12" md="4" sm="6" xl="3">
+                        <nombramiento-card :nombramiento="item"></nombramiento-card>
+                    </v-col>
+                </template>
+            </v-row>
+            <v-row>
+                <v-col cols="12">
+                    <pagination @loading="toggleLoading"></pagination>
                 </v-col>
-            </template>
-        </v-row>
-        <v-row>
-            <v-col cols="12">
-                <pagination @loading="toggleLoading"></pagination>
-            </v-col>
-        </v-row>
+            </v-row>
+        </template>
+        <template v-else>
+            <v-row>
+                <v-col cols="12">
+                    No existen titulaciones
+                </v-col>
+            </v-row>
+        </template>
         <v-overlay :value="overlay">
             <v-progress-circular indeterminate size="64"></v-progress-circular>
         </v-overlay>
@@ -19,12 +28,12 @@
 </template>
 
 <script>
-    import {mapState} from 'vuex';
-    import NombramientoCard from "../../components/titulaciones/NombramientoCardComponent";
-    import Pagination from "../../components/PaginationComponent";
+    import {mapActions, mapGetters, mapState} from 'vuex';
+    import NombramientoCard from '../../components/titulaciones/NombramientoCardComponent';
+    import Pagination from '../../components/PaginationComponent';
 
     export default {
-        name: "Index",
+        name: 'Index',
         components: {
             Pagination,
             NombramientoCard
@@ -36,46 +45,36 @@
                 overlay: false,
             }
         },
-        beforeRouteEnter(to, from, next) {
-            next(vm => {
-                vm.loading = true;
-                vm.overlay = true;
-                vm.$store.dispatch('titulaciones/fetchTitulaciones', {page: 1})
-                    .then(res => {
-                        vm.loading = false;
-                        vm.overlay = false;
-                        next();
-                    })
-                    .catch(err => {
-                        vm.loading = false;
-                        vm.overlay = false;
-                        next();
-                    })
-            })
-        },
-        beforeRouteUpdate(to, from, next) {
-            this.loading = true;
-            this.overlay = true;
-            this.$store.dispatch('titulaciones/fetchTitulaciones', {page: 1})
+        created() {
+            this.loading = true
+            this.overlay = true
+            this['titulaciones/fetchTitulaciones']({page: 1})
                 .then(res => {
                     this.loading = false;
                     this.overlay = false;
-                    next();
                 })
                 .catch(err => {
                     this.loading = false;
                     this.overlay = false;
-                    next();
-                });
+                })
         },
         methods: {
             toggleLoading(toggle) {
                 this.loading = toggle;
                 this.overlay = toggle;
             },
+            ...mapActions([
+                'titulaciones/fetchTitulaciones'
+            ]),
+            ...mapGetters([
+                'titulaciones/lengthNombramientos'
+            ])
         },
         watch: {},
         computed: {
+            lengthNombramientos() {
+                return this['titulaciones/lengthNombramientos']()
+            },
             ...mapState({
                 titulaciones: state => state.titulaciones.titulaciones
             })
